8d8b04c9f162a649016e22d7cd0d2015ec9d4b1c
Drews777 <57402415+Drews777@users.noreply.github.com>
2021-04-23 10:32:08 +0600

    Update script.js

--- usefull staff/change html-tag/script.js
+++ usefull staff/change html-tag/script.js
@@ -3,3 +3,9 @@
             return "<div>" + $(this).text() + "</div>";
         });
     });
+//    Отставляет весь внутренний контент с вложенными html-тегами
+    $(function() {
+        $('h3').replaceWith(function() {
+            return "<div>" + $(this).html() + "</div>";
+        });
+    });
