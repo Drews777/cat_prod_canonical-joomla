e0aa7c1bd795d505d7680c1a9b278f471408fc49
Drews777 <57402415+Drews777@users.noreply.github.com>
2021-06-03 10:53:08 +0600

    Add files via upload

---
+++ usefull staff/delete text-javascript/script.php
@@ -0,0 +1,7 @@
+add_action( 'template_redirect', function(){
+    ob_start( function( $buffer ){
+        $buffer = str_replace( array( 'type="text/javascript"', "type='text/javascript'" ), '', $buffer );
+        $buffer = str_replace( array( 'type="text/css"', "type='text/css'" ), '', $buffer );
+        return $buffer;
+    });
+});
